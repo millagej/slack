0af36a7aaf4a4e00fb7a30e3bb2e9dbce1f13e64
joshua.c.millage <millagej@icloud.com>
2020-05-08 15:25:12 -0400

    add LogCall.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -2,7 +2,7 @@ package day_1;
 
 public class CreateChannel {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("Channel fuction is working");
 
     }
 }
